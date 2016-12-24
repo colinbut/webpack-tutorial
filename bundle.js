fa143093129c3c5302b6767f3b4d550d4eda4c72
Colin But <colinbut@users.noreply.github.com>
2016-12-24 10:06:19 +0000

    updated bundle.js including CSS

--- bundle.js
+++ bundle.js
@@ -44,6 +44,8 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	//require("!style!css!./style.css");
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./style.css\""); e.code = 'MODULE_NOT_FOUND'; throw e; }())); // using binding loaders
 	document.write(__webpack_require__(1));
 
 
